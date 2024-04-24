3003ae9b9e7530fa7392d932cc145fce6d023521
Erichards01 <erichards066@gmail.com>
2024-04-24 19:18:43 +0800

    transfer code from old project and fix errors

---
+++ app/components/Footer.js
@@ -0,0 +1,65 @@
+"use client";
+
+import footerImage from "../../public/LawnFooter.jpg";
+import React from "react";
+import Image from "next/image";
+import logo from "../../public/LogoComplete.jpg";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+export default function Footer() {
+  const router = useRouter();
+  return (
+    <div>
+      <footer>
+        <Image src={footerImage} alt="grass" className="footerImage" />
+        <Image
+          src={logo}
+          alt="lawn mowing logo"
+          placeholder="blur"
+          className="logo"
+        />
+        <ul>
+          <li className="foot">
+            <Link
+              className={router.pathname == "/" ? "footerActive" : ""}
+              href="/"
+            >
+              Home
+            </Link>
+          </li>
+          <li className="foot">
+            <Link
+              className={router.pathname == "/about" ? "footerActive" : ""}
+              href="/about"
+            >
+              About
+            </Link>
+          </li>{" "}
+          <li className="foot">
+            <Link
+              className={router.pathname == "/services" ? "footerActive" : ""}
+              href="/services"
+            >
+              Services
+            </Link>
+          </li>{" "}
+          <li className="foot">
+            <Link
+              className={router.pathname == "/contact" ? "footerActive" : ""}
+              href="/contact"
+            >
+              Contact
+            </Link>
+          </li>{" "}
+          {/* <a
+          href="https://www.facebook.com/Perthgardenandlawn"
+          className="footerFacebook"
+        >
+          <Image src={facebook} alt="facebook" className="Facebook" />
+        </a> */}
+        </ul>
+      </footer>
+    </div>
+  );
+}
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./styles/globals.css";
 import Navbar from "./components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
+import Footer from "./components/Footer";
 
 export const metadata = {
   title: "Create Next App",
@@ -10,10 +11,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <div>
-      <Navbar />
-
-      {children}
+    <div className="container">
+      <div className={inter.className}>
+        <Navbar />
+        <div className="content-wrap"> {children}</div>
+        <Footer />
+      </div>
     </div>
   );
 }
