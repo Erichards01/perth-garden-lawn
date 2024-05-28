68634261edd4063ba99a00e663ca287876149fd6
Erichards01 <erichards066@gmail.com>
2024-05-28 21:11:18 +0800

    Lawnmower photo added to services

--- app/services/page.js
+++ app/services/page.js
@@ -1,7 +1,8 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
 import HeaderAnimation from "../components/HeaderAnimation";
-
+import Image from "next/image";
+import LawnMower1 from "../../public/lawnmower1.jpg";
 const header = "Services"
 
 export default function services() {
@@ -10,7 +11,13 @@ export default function services() {
       <title>{header}</title>
       <HeaderAnimation  header={header}/>
       <div className={styles.indexContent}>
-      <p>Cylinder & Rotary Mowing </p> 
+      <p>Cylinder & Rotary Mowing </p> <Image 
+      src={LawnMower1}    alt=""
+      className={styles.content}
+      style={{ height: "300px", width: "auto", marginRight: "20px" }}
+      />
+    
+      
       <p>Gardending/Landscaping </p> 
       <p>Green Waste/Rubbish</p>
       <p>Removal Garden Bag Service</p> 
