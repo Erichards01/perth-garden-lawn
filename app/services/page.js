f07a6deefe3243d0b59ac42a7d0482194c744180
Erichards01 <erichards066@gmail.com>
2024-05-29 21:02:18 +0800

    services pics added and flex started

--- app/services/page.js
+++ app/services/page.js
@@ -3,24 +3,62 @@ import styles from "../styles/Home.module.css";
 import HeaderAnimation from "../components/HeaderAnimation";
 import Image from "next/image";
 import LawnMower1 from "../../public/lawnmower1.jpg";
+import LawnMower2 from "../../public/Lawnmower2.jpeg";
+import Planting from "../../public/Planting1.jpeg"
+import Gardening from "../../public/Gardening2.jpg"
+import GarbageCan from "../../public/GarbageCan.jpg"
+import GardenBag from "../../public/GardenBag1.jpg"
+import GreenWaste from "../../public/GreenWaste.jpg"
+import Boxes from "../../public/RemovalBoxes.jpeg"
+
 const header = "Services"
 
 export default function services() {
   return (
     <>
       <title>{header}</title>
-      <HeaderAnimation  header={header}/>
-      <div className={styles.indexContent}>
-      <p>Cylinder & Rotary Mowing </p> <Image 
-      src={LawnMower1}    alt=""
-      className={styles.content}
-      style={{ height: "300px", width: "auto", marginRight: "20px" }}
-      />
-    
       
-      <p>Gardending/Landscaping </p> 
-      <p>Green Waste/Rubbish</p>
-      <p>Removal Garden Bag Service</p> 
+      <HeaderAnimation  header={header}/>
+      
+      <div className={styles.servicesContent}><div className={styles.row}><Image 
+      src={LawnMower1}    alt="Rotary Mower"
+      className={styles.servicePics}
+      
+      />
+      <p>Cylinder & Rotary Mowing </p> <Image 
+      src={LawnMower2}    alt="Cylinder Mower"
+      className={styles.servicePics}
+      
+      />
+    </div><div className={styles.row}>
+    <Image 
+      src={Planting}    alt="Plant pulled out of pot"
+      className={styles.servicePics}
+      
+      />
+      <p>Gardending/Landscaping </p> <Image 
+      src={Gardening}    alt="Hedge trimming hedge"
+      className={styles.servicePics}
+      
+      /></div><div className={styles.row}> <Image 
+      src={GreenWaste}    alt="Lawn clippings in bin"
+      className={styles.servicePics}
+      
+      /> <p>Green Waste Removal</p> <Image 
+      src={GardenBag}    alt="Lawn clippings in garden bag"
+      className={styles.servicePics}
+      
+      /></div><div className={styles.row}>
+      <Image 
+      src={Boxes}    alt="Cardboard boxes in truck"
+      className={styles.servicePics}
+      
+      /><p>Rubbish Removal</p><Image 
+      src={GarbageCan}    alt="Green garbage can"
+      className={styles.servicePics}
+      
+      />
+    </div>
       </div>
     </>
   );
